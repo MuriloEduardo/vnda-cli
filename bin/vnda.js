3340695e758f29392f8729b39fa7f9ab462eeede
˜MuriloEduardo˜ <muriloeduardoooooo@gmail.com>
2018-05-11 00:34:14 -0300

    Save change

--- bin/vnda.js
+++ bin/vnda.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
  
 const lib = require('../lib/index.js');
-const greeting = lib.sayHello('Bret');
+const gitlab = require('node-gitlab');
+
+const client = gitlab.create({
+    api: 'https://gitlab.com/api/v4',
+    privateToken: '4ScWD4byz8hNZYirX28u'
+});
+
 const [,, ...args] = process.argv;
+
+client.repositoryBranches.list({id: 'front'})
+    .then(function (milestones) {
+        console.log(milestones);
+    })
+    .catch(function (err) {
+        throw err;
+    });
  
-console.log(args);
+console.log(lib.sayHello(args));
